c73437605a62ed3d2e4e995b4151224a6b16ed5a
KrithikShailesh <krithikshaileshr@gmail.com>
2021-06-07 15:44:23 +0530

    07/06/2021

---
+++ JavaScript/Basic_Algorithm_Scripting/Largest_Numbers.js
@@ -0,0 +1,17 @@
+/*
+Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+
+Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+*/
+
+function largestOfFour(arr) {
+    let array=[];
+  
+    for(let i=0;i<arr.length;i++){
+      array.push(Math.max(...arr[i]));
+    }
+    
+    return array;
+  }
+  
+  console.log(largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
